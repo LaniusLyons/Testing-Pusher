d524b208d149f50a950ee121c02c21e0c3746503
FaustoMora <patitodelt22@gmail.com>
2017-04-01 18:30:13 -0500

    working pusher

--- MyPusherSite/core/views.py
+++ MyPusherSite/core/views.py
@@ -1,4 +1,3 @@
-import pusher
 from django.shortcuts import render
 from .models import *
 from django.contrib.auth import authenticate
@@ -8,15 +7,24 @@ from django.contrib.auth import logout as auth_logout
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.conf import settings
 
+def module_exists(module_name):
+	try:
+		__import__('pusher')
+	except ImportError:
+		return False
+	else:
+		return True
 
-# pusher_client = pusher.Pusher(
-# 	app_id=settings.PUSHER_APP_ID,
-# 	key=settings.PUSHER_KEY,
-# 	secret=settings.PUSHER_SECRET,
-# 	ssl=True
-# )	
-# pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
+
 # Create your views here.
 
 def index(request):
+	pusher = __import__('pusher')
+	pusher_client = pusher.Pusher(
+		app_id=settings.PUSHER_APP_ID,
+		key=settings.PUSHER_KEY,
+		secret=settings.PUSHER_SECRET,
+		ssl=True
+	)	
+	pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
 	return render(request,'index.html',{'key':settings.PUSHER_KEY})
